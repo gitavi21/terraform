2bfeb278b8f38afbd3357df9b48b57f340dad807
gitavi21 <gitavi2108@gmail.com>
2023-02-17 12:45:05 +0530

    variables

--- m1.tf
+++ m1.tf
@@ -1,12 +1,34 @@
 provider "azurerm" {
-    features {
-      
-    }
-  
+    features {}
 }
-
-resource "azurerm_resource_group" "bajaj-local" {
-    location = "eastus"
-    name="terra"
-  
+variable "location" {
+  default = "eastus"
+}
+variable "name" {
+  default = "newrs1"
+}
+resource "azurerm_resource_group" "resvar" {
+  location = var.location
+  name = var.name
+  tags = {
+    Name = "Aviral"
+    Day = "Friday"
+  }
+}
+resource "azurerm_cosmosdb_account" "GlobalDocumentDB" {
+  name                      = var.name
+  location                  = var.location
+  resource_group_name       = azurerm_resource_group.resvar.name
+  offer_type                = "Standard"
+  kind                      = "GlobalDocumentDB"
+  enable_automatic_failover = false
+  geo_location {
+    location          = var.location
+    failover_priority = 0
+  }
+  consistency_policy {
+    consistency_level       = "BoundedStaleness"
+    max_interval_in_seconds = 300
+    max_staleness_prefix    = 100000
+  }
 }
